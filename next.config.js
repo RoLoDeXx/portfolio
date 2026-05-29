28faae96fafd8cb9022759a9626bf3f8d7889218
RoLoDeXx <imailtosamarth@gmail.com>
2026-05-30 05:23:20 +0530

    Remove GitHub Pages config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-  ...(isProd ? { output: 'export' } : {}),
-  basePath: isProd ? '/portfolio' : '',
   images: { unoptimized: true },
 }
 module.exports = nextConfig
