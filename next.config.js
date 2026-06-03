1e5172f6bd8ad706d80cef0b086695f94c13232f
RoLoDeXx <imailtosamarth@gmail.com>
2026-06-03 15:14:36 +0530

    Update: deploy configs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+  ...(isProd ? { output: 'export' } : {}),
   images: { unoptimized: true },
 }
 module.exports = nextConfig
